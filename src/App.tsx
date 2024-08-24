1235155aa684c33d7107683eb4e2572530ccfb0d
MenatalhHossamalden <146476199+MenaTalhHossamAlden@users.noreply.github.com>
2024-08-24 14:11:30 +0300

    Cleanup the Project

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+// main .tsx file rendered on the screen
+import React from 'react';
+import HomeScreen from './screens/HomeScreen';
+
+const App = () => {
+  return <HomeScreen></HomeScreen>;
+};
+
+export default App;
